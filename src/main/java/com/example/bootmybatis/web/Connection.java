579a413f60cdc751eeb48780e045cb8d519dbba6
huyaou <huyaou@baiwang.com>
2018-07-14 21:21:37 +0800

    commit Connection class

---
+++ src/main/java/com/example/bootmybatis/web/Connection.java
@@ -0,0 +1,11 @@
+package com.example.bootmybatis.web;
+
+/**
+ * @Author: baiwang-097
+ * @Date: 2018/7/14 21:20
+ * @Description: //TODO
+ * @Version: 1.0
+ */
+public class Connection {
+    //新建类，测试提交到GitHub
+}
