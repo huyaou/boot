524068124959c3f6a78e97f986d0d0976679888b
huyaou <huyaou@baiwang.com>
2018-07-16 10:07:24 +0800

    add Getter and setter

--- src/main/java/com/example/bootmybatis/web/Connection.java
+++ src/main/java/com/example/bootmybatis/web/Connection.java
@@ -15,4 +15,36 @@ public class Connection {
     private String password;
 
     private String driver;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
 }
